05f676093a3420809e22e02d9962b03d297ace28
SAMIR-CLOUD <samirkumargouda0563@gmail.com>
2026-01-09 13:44:06 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,7 +1,7 @@
-const API_BASE_URL = "http://34.200.246.84:84"  // For reverse proxy it is mandatory so dont change
+//const API_BASE_URL = "http://34.200.246.84:84"  // For reverse proxy it is mandatory so dont change
 
 // const API_BASE_URL = "http://98.92.112.243:84"
-//const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
+const API_BASE_URL = "http://a799c807761e04ca3b32071f4c8b4d4b-1894326519.us-east-1.elb.amazonaws.com";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
